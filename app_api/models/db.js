5927deb4f057d704e2dfef498c18f373a4e2f4f8
Jonatas Leon <jonatas.leon@gmail.com>
2016-08-27 18:05:06 -0300

    Setup models integrations

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,4 +1,8 @@
+var fs = require('fs');
+var path = require('path');
+
 var Sequelize = require('sequelize');
+var db = {};
 
 var sequelize = new Sequelize('sw_energy', 'root', 'aluno', {
   host: 'localhost',
@@ -9,3 +13,24 @@ var sequelize = new Sequelize('sw_energy', 'root', 'aluno', {
     idle: 10000
   }
 });
+
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf(".") !== 0) && (file !== "index.js");
+  })
+  .forEach(function(file) {
+    var model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
