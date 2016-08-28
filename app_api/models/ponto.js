f87ce2900ebb81210548fefcc1578d42c4f3947b
Jonatas Leon <jonatas.leon@gmail.com>
2016-08-27 23:32:33 -0300

    Fix minor mistakes

--- app_api/models/ponto.js
+++ app_api/models/ponto.js
@@ -3,9 +3,9 @@ module.exports = function(sequelize, DataTypes) {
   var Ponto = sequelize.define('Ponto', {
     serial: DataTypes.INTEGER,
     placa: DataTypes.STRING,
-    versao: DataTypes.FLOAT
+    versao: DataTypes.FLOAT,
     estado: {
-      type: Sequelize.INTEGER,
+      type: DataTypes.INTEGER,
       defaultValue: 0
     },
   }, {
