e18198fa194a2cb1b4cdb772837d6ac1ea522aae
Jonatas Leon <jonatas.leon@gmail.com>
2016-08-27 23:09:57 -0300

    Implements addPonto e getAllPontos

--- app_api/controllers/index.js
+++ app_api/controllers/index.js
@@ -1,4 +1,19 @@
-module.exports.demo = function(req, res, next) {
-  console.log(req.query);
-  res.send('');
+var models = require('../models');
+
+module.exports.getAllPontos = function(req, res, next) {
+  models.Ponto.findAll().then(function(pontos) {
+    res.json(pontos);
+  });
+};
+
+module.exports.addPonto = function(req, res, next) {
+  var ponto = {
+    serial: req.query['SERIAL'],
+    placa: req.query['PLACA'],
+    versao: req.query['VERSAO']
+  };
+
+  models.Ponto.create(ponto).then(function(ponto) {
+    res.json(ponto);
+  });
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var index = require('../controllers/app.js');
+var index = require('../controllers');
 
-router.get('/', index.demo);
+router.get('/pontos/', index.getAllPontos);
+router.get('/pontos/add/', index.addPonto);
+router.get('/pontos/state/', function(req, res, next) {
+  res.send('value=255');
+});
 
 module.exports = router;
