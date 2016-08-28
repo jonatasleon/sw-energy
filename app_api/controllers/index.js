99ac57c5e580cda042d3262a5e5e0128e1bc5800
Jonatas Leon <jonatas.leon@gmail.com>
2016-08-28 05:26:51 -0300

    Fecha parte da api

--- app_api/controllers/index.js
+++ app_api/controllers/index.js
@@ -22,12 +22,73 @@ module.exports.getState = function(req, res, next) {
   models.Ponto
     .findOne({
       where: {
-        serial: req.query['SERIAL']
+        serial: parseInt(req.query['SERIAL'], 10)
       }
     })
     .then(function(ponto) {
-      if(!ponto)
-        ponto = {estado: 0};
-      res.json(ponto.estado);
+      if (!ponto)
+        ponto = {
+          estado: 0
+        };
+      res.json('value=' + ponto.estado);
+    });
+};
+
+module.exports.setState = function(req, res, next) {
+  var n = parseInt(req.body['SERIAL'], 10);
+
+  models.Ponto
+    .findOne({
+      where: {
+        serial: n
+      }
+    })
+    .then(function(ponto) {
+      if (!ponto) {
+        res.status(400).json({
+          msg: 'ERROR'
+        });
+        return;
+      }
+
+      console.log(ponto);
+
+      ponto.estado = parseInt(req.body['ESTADO'], 10);
+      ponto.save();
+      res.json(ponto);
+    });
+};
+
+// corrent: DataTypes.FLOAT,
+// tensao: DataTypes.FLOAT,
+// tempo: DataTypes.DATE
+
+module.exports.setLeitura = function(req, res, next) {
+  console.log('query', req.query);
+  models.Leitura
+    .create({
+      corrent: parseFloat(req.query['CORRENTE']),
+      tensao: parseFloat(req.query['TENSAO']),
+      tempo: Date.now()
+    })
+    .then(function(leitura) {
+    });
+};
+
+module.exports.getLastLeitura = function(req, res, next) {
+  models.Leitura
+    .findOne({
+      attributes: ['corrent', 'tensao', 'tempo'],
+      order: [
+        ['id', 'DESC']
+      ]
+    })
+    .then(function(leitura) {
+      var d = {};
+      d.corrente = leitura.corrent;
+      d.tensao = leitura.tensao;
+      d.tempo = leitura.tempo;
+
+      res.json(d);
     });
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,5 +5,9 @@ var index = require('../controllers');
 router.get('/pontos/', index.getAllPontos);
 router.get('/pontos/add/', index.addPonto);
 router.get('/pontos/state/', index.getState);
+router.post('/pontos/state/', index.setState);
+
+router.get('/leituras/last', index.getLastLeitura);
+router.get('/leituras/', index.setLeitura);
 
 module.exports = router;
